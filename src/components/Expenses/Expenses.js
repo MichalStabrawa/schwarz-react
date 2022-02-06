bb16488d4d201732f7365627a0aba14e25e6a050
MichalStabrawa <mistaaqua@gmail.com>
2022-02-06 21:20:35 +0100

    add filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,9 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
     return (
         <div>
             <Card className="expenses">
@@ -19,7 +22,7 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {props.items.map((expense) => (
+                {filteredExpenses.map((expense) => (
                     <ExpenseItem
                         title={expense.title}
                         amount={expense.amount}
