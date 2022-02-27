d1fa9d71e9bc9bed6ec78f6fc2f655512929a0e5
MichalStabrawa <mistaaqua@gmail.com>
2022-02-27 23:07:47 +0100

    add ul

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,26 @@
+import react from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpensesList = (props) => {
+    let expensesContent = <p>No expenses found</p>;
+
+    if (props.items.length === 0) {
+        return <h2 className="expenses-list-fallback">Found no expenses</h2>;
+    }
+
+    return (
+        <ul className="expenses-list">
+            {props.items.map((expense) => (
+                <ExpenseItem
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                    key={expense.id}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default ExpensesList;
